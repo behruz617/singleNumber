d464ab6e89097e9a1d8eac182a7f37c0561c5126
Behruz Nesirli <behruznesirli90@gmail.com>
2024-11-13 16:04:49 +0400

    Deyisiklik

--- src/main.java
+++ src/main.java
@@ -1,16 +1,16 @@
 public class main {
     public static void main(String[] args) {
         int[] nums1 = {2, 2, 1};
-        System.out.println(singleNumber(nums1)); // Output: 1
+        System.out.println(singleNumbers(nums1)); // Output: 1
 
         int[] nums2 = {4, 1, 2, 1, 2};
-        System.out.println(singleNumber(nums2)); // Output: 4
+        System.out.println(singleNumbers(nums2)); // Output: 4
 
         int[] nums3 = {1};
-        System.out.println(singleNumber(nums3)); // Output: 1
+        System.out.println(singleNumbers(nums3)); // Output: 1
     }
 
-    public static int singleNumber(int[] nums) {
+    public static int singleNumbers(int[] nums) {
         int result = 0;
 
         for (int num : nums) {
